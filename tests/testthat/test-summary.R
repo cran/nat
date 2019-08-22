99f84318ddfb17a5bcc81061ab270dd718634957
Gregory Jefferis <jefferis@gmail.com>
2019-08-22 14:30:02 +0000

    version 1.8.13

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -1,6 +1,7 @@
 context("summary")
 
 test_that("summary.neuronlist behaves", {
-  expect_is(summary(Cell07PNs), 'data.frame')
+  expect_is(s <- summary(Cell07PNs), 'data.frame')
+  expect_equal_to_reference(s, file = 'testdata/summary_cell07pns.rda', tolerance=1e-3)
   expect_is(summary(kcs20, veclength=1.2), 'data.frame')
 })
